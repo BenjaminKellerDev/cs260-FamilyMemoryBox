2c4f7f5614822348aa977995d361383cf8877a30
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-09 23:05:32 -0700

    converted tags

--- src/stories/stories.jsx
+++ src/stories/stories.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './stories.css';
 
 export function Stories() {
     return (
--- src/tags/tags.jsx
+++ src/tags/tags.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import './tags.css';
 
 export function Tags() {
     return (
-        <main className="container-fluid bg-secondary text-center">
-            <div>login displayed here</div>
+        <main>
+            <section>
+                <h1>Current Tags:</h1>
+                <div id="tagContainer">
+                    <span className="tag">Grandma ❌</span> <span className="tag">Europe ❌</span> <span className="tag">Family-Friend
+                        Frank ❌</span>
+                </div>
+            </section>
+            <form action="tags.html">
+                <input type="text" id="tagBox" name="newTag" className="shrink form-control" placeholder="Tag Name" />
+                <button type="submit" className="btn btn-secondary">Add Tag</button>
+            </form>
         </main>
     );
 }
