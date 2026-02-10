0a58a27b62474877a4e9d22c0febf714ea15ed56
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-09 23:01:23 -0700

    added router

--- src/app.jsx
+++ src/app.jsx
@@ -8,26 +8,38 @@ import { Stories } from './stories/stories';
 import { Tags } from './tags/tags';
 
 export default function App() {
-    return <div className="body">
-        <header>
-            <nav>
-                <NavLink to="/stories.html" className="headerLink"><span className="ultraCompactHidable">Story </span>Feed</NavLink >
-                <NavLink to="/tags.html" className="headerLink"><span className="ultraCompactHidable">Manage
-                </span>Tags</NavLink >
-                <NavLink to="/index.html" className="headerLink">Logout</NavLink >
-            </nav>
-            <div id="usernameHeader">
-                <span id="usernameText">Logged in as:</span> <span id="usernameTag">username</span>
-            </div>
-        </header>
+    return <BrowserRouter>
+        <div className="body">
+            <header>
+                <nav>
+                    <NavLink to="/stories" className="headerLink"><span className="ultraCompactHidable">Story </span>Feed</NavLink>
+                    <NavLink to="/tags" className="headerLink"><span className="ultraCompactHidable">Manage
+                    </span>Tags</NavLink>
+                    <NavLink to="/login" className="headerLink">Logout</NavLink>
+                </nav>
+                <div id="usernameHeader">
+                    <span id="usernameText">Logged in as:</span> <span id="usernameTag">username</span>
+                </div>
+            </header>
 
-        <main>component here</main>
+            <Routes>
+                <Route path='/' element={<Login />} exact />
+                <Route path='/drafting' element={<Drafting />} />
+                <Route path='/stories' element={<Stories />} />
+                <Route path='/tags' element={<Tags />} />
+                <Route path='*' element={<NotFound />} />
+            </Routes>
 
 
-        <footer>
-            <p>by Benjamin Keller</p>
-            <a href=" https://github.com/BenjaminKellerDev/cs260-FamilyMemoryBox">Source</a>
-        </footer>
+            <footer>
+                <p>by Benjamin Keller</p>
+                <a href=" https://github.com/BenjaminKellerDev/cs260-FamilyMemoryBox">Source</a>
+            </footer>
 
-    </div>;
+        </div>
+    </BrowserRouter >;
+}
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
