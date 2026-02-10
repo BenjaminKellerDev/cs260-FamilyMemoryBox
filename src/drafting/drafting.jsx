29f7a0e93c6e2105f02d1b0f2d3eecfc2a0cf140
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-09 23:26:42 -0700

    converted login and fixed drafting  except for button on login

--- src/drafting/drafting.jsx
+++ src/drafting/drafting.jsx
@@ -7,34 +7,32 @@ export function Drafting() {
                 <label for="title">
                     <h2>Title:</h2>
                 </label>
-                <input type="text" id="title" name="title" class="shrink form-control" />
+                <input type="text" id="title" name="title" className="shrink form-control" />
                 <fieldset id="tagContainer">
                     <legend>
-                        <h4 class="padding10px">Select Tags:</h4>
+                        <h4 className="padding10px">Select Tags:</h4>
                     </legend>
-                    <label class="tag">
+                    <label className="tag">
                         <span for="tag1">Grandma</span>
                         <input type="checkbox" id="tag1" name="Grandma" value="Grandma"></input>
                     </label>
-                    <label class="tag">
+                    <label className="tag">
                         <span for="tag1">Europe</span>
                         <input type="checkbox" id="tag2" name="Europe" value="Europe"></input>
                     </label>
-                    <label class="tag">
+                    <label className="tag">
                         <span for="tag1">Family-Friend Frank</span>
                         <input type="checkbox" id="tag3" name="Family-Friend Frank" value="Family-Friend Frank"></input>
                     </label>
                 </fieldset>
                 <label for="storyBox">
-                    <h4 class="padding10px">Story:</h4>
+                    <h4 className="padding10px">Story:</h4>
                 </label>
                 <br></br>
-                <textarea class="form-control" id="storyBox" name="storyBox"></textarea>
+                <textarea className="form-control" id="storyBox" name="storyBox"></textarea>
                 <br></br>
-                <button type="submit" class="btn btn-secondary">Upload</button>
+                <button type="submit" className="btn btn-secondary">Upload</button>
             </form>
-
-
         </main>
     );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 
 export function Login() {
     return (
-        <main className="container-fluid bg-secondary text-center">
-            <div>login displayed here</div>
+        <main>
+            <div id="loginBox">
+                <h1>Welcome to your digital</h1>
+                <h1> <i>Memory Box</i></h1>
+
+                <form action="stories.html">
+                    <div className="inputBox">
+                        <label for="nameBox" className="form-label">Name:</label>
+                        <input type="text" id="nameBox" name="loginName" className="shrink form-control" />
+                    </div>
+                    <div className="inputBox">
+                        <label for="emailBox" className="form-label">Email:</label>
+                        <input type="email" id="emailBox" name="loginEmail" className="shrink form-control" />
+                    </div>
+                    <div className="inputBox">
+                        <label for="passwordBox" className="form-label">Password:</label>
+                        <input type="password" id="passwordBox" name="loginPassword" className="shrink form-control" />
+                    </div>
+                    <div>
+                        <button type="submit" className="btn btn-primary" style="margin: 10px;">Login</button>
+                        <button type="submit" className="btn btn-secondary" style="margin: 10px;">Create</button>
+                    </div>
+                </form>
+            </div>
+            <div id="artBox">
+                <h4>Inspirational art for your enjoyment:</h4>
+                <a href=" https://www.artic.edu/artworks/20684/paris-street-rainy-day">
+                    <img src="https://www.artic.edu/iiif/2/f8fd76e9-c396-5678-36ed-6a348c904d27/full/1686,/0/default.jpg"
+                        alt="painting" className="img"></img>
+                </a>
+            </div>
         </main>
     );
 }
