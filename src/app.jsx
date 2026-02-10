64322f69a334ec409ba7c4bf8c5b6aaa9d5fb426
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-09 22:43:05 -0700

    first header and footer

---
+++ src/app.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+export default function App() {
+    return <div className="body bg-dark text-light">
+        <header>
+            <nav>
+                <a href="/stories.html" className="headerLink"><span className="ultraCompactHidable">Story </span>Feed</a>
+                <a href="/tags.html" className="headerLink"><span className="ultraCompactHidable">Manage
+                </span>Tags</a>
+                <a href="/index.html" className="headerLink">Logout</a>
+            </nav>
+            <div id="usernameHeader">
+                <span id="usernameText">Logged in as:</span> <span id="usernameTag">username</span>
+            </div>
+        </header>
+
+        <footer>
+            <p>by Benjamin Keller</p>
+            <a href=" https://github.com/BenjaminKellerDev/cs260-FamilyMemoryBox">Source</a>
+        </footer>
+
+    </div>;
+}
