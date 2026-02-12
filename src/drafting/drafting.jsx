4a14b0d31d22c8356d83adcbeb2c45a167176edf
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-11 18:47:44 -0700

    fixed visual bug that was annoying me

--- src/drafting/drafting.jsx
+++ src/drafting/drafting.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../tags/tags.css';
 
 export function Drafting() {
     return (
@@ -13,15 +14,15 @@ export function Drafting() {
                         <h4 className="padding10px">Select Tags:</h4>
                     </legend>
                     <label className="tag">
-                        <span for="tag1">Grandma</span>
+                        <span for="tag1" className="padding3px">Grandma</span>
                         <input type="checkbox" id="tag1" name="Grandma" value="Grandma"></input>
                     </label>
                     <label className="tag">
-                        <span for="tag1">Europe</span>
+                        <span for="tag1" className="padding3px">Europe</span>
                         <input type="checkbox" id="tag2" name="Europe" value="Europe"></input>
                     </label>
                     <label className="tag">
-                        <span for="tag1">Family-Friend Frank</span>
+                        <span for="tag1" className="padding3px">Family-Friend Frank</span>
                         <input type="checkbox" id="tag3" name="Family-Friend Frank" value="Family-Friend Frank"></input>
                     </label>
                 </fieldset>
