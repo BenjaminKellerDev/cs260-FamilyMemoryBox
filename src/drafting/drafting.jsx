ab6b7e162660222ba2ac39a3f3fdf4ec20cb07c8
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-24 22:09:19 -0700

    trying to link checkboxes to jsx

--- src/drafting/drafting.jsx
+++ src/drafting/drafting.jsx
@@ -4,11 +4,24 @@ import '../tags/tags.css';
 import { getTagsFromDatabase, getStoriesFromDB, addNewStoryToDB } from '../database'
 
 export function Drafting() {
+
+    const [tags, setTags] = React.useState([]);
     React.useEffect(() => {
         setTags(getTagsFromDatabase())
     }, []);
 
-    const [tags, setTags] = React.useState([]);
+    const [title, setTitle] = React.useState('');
+    const [storyTags, setStoryTags] = React.useState([]);
+    const [story, setStory] = React.useState('');
+
+    function toggleTag(e) {
+        const tagText = e.target.name;
+        if (storyTags.includes(tagText)) {
+            setStoryTags(storyTags.filter(t => t != tagText));
+        } else {
+            setStoryTags([...storyTags, tagText]);
+        }
+    }
 
     return (
         <main>
@@ -16,7 +29,7 @@ export function Drafting() {
                 <label htmlFor="title">
                     <h2>Title:</h2>
                 </label>
-                <input type="text" id="title" name="title" className="shrink text-form-control form-control" />
+                <input type="text" id="title" name="title" className="shrink text-form-control form-control" onChange={(e) => setTitle(e.target.value)} />
                 <fieldset id="tagContainer">
                     <legend>
                         <h4 className="padding10px">Select Tags:</h4>
@@ -27,7 +40,7 @@ export function Drafting() {
                     <h4 className="padding10px">Story:</h4>
                 </label>
                 <br></br>
-                <textarea className="text-form-control form-control" id="storyBox" name="storyBox"></textarea>
+                <textarea className="text-form-control form-control" id="storyBox" name="storyBox" onChange={(e) => setStory(e.target.value)}></textarea>
                 <br></br>
                 <button type="submit" className="btn btn-secondary">Upload</button>
             </form>
@@ -37,7 +50,7 @@ export function Drafting() {
     function Tag({ name }) {
         return <label className="tag">
             <span htmlFor="tag1" className="padding3px">{name}</span>
-            <input type="checkbox" id="tag3" name="Family-Friend Frank" value="Family-Friend Frank"></input>
+            <input type="checkbox" onClick={toggleTag} name={name}/* id="tag3" name="Family-Friend Frank" value="Family-Friend Frank" */></input>
         </label>
     }
 }
