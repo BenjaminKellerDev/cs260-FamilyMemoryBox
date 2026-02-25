30fd4df1d14d68aaac71d149369a1a54e907e314
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-24 23:47:43 -0700

    started to componentize stories

--- src/stories/stories.jsx
+++ src/stories/stories.jsx
@@ -5,60 +5,32 @@ import { useNavigate } from 'react-router-dom';
 import { getStoriesFromDB } from '../database'
 
 export function Stories() {
+    const navigate = useNavigate();
     return (
         <main>
             <div className="buttonContainer">
                 <form action="drafting">
-                    <button type="submit" className="btn btn-secondary">New Story</button>
+                    <button type="submit" className="btn btn-secondary" onClick={(e) => { e.preventDefault(); navigate('../drafting'); }}>New Story</button>
                 </form>
                 <button className="newPostBtn btn btn-primary">New Posts! refresh now</button>
             </div>
-            {/*  <!-- story 1--> */}
+            <div>no content</div>
+
+
+        </main>
+    );
+
+    function Story({ storyOBJ }) {
+        return (
             <div className="story">
                 <hr></hr>
-                <h2 className="text-aline-center">That one time I went to that one place</h2>
-                <h5 className="text-aline-center">By Grandma</h5>
+                <h2 className="text-aline-center">{storyOBJ.title}</h2>
+                <h5 className="text-aline-center">By {storyOBJ.author}</h5>
                 <p className="tagContainer"><span className="tagTitleSpacing"><i>Tags: </i></span><span
                     className="tag">Grandma</span> <span className="tag">Europe</span> <span className="tag">Family-Friend
                         Frank</span>
                 </p>
-                <p className="leftElement">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. </p>
+                <p className="leftElement">{storyOBJ.story} </p>
                 <hr></hr>
                 <section className="outOfFocus">
                     <h3>Comments</h3>
@@ -84,66 +56,10 @@ export function Stories() {
                 </section>
             </div>
 
-            {/*  <!-- story 2--> */}
-            <div className="story">
-                <hr></hr>
-                <h2 className="text-aline-center">The time I went to that one place, again!!</h2>
-                <h5 className="text-aline-center">By Grandma</h5>
-                <p className="tagContainer"><span className="tagTitleSpacing"><i>Tags: </i></span><span
-                    className="tag">Grandma</span>
-                    <span className="tag">Europe</span>
-                </p>
-                <p className="leftElement">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa
-                    repellat iure vel ipsum
-                    nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id?
-                    Natus, illum excepturi. </p>
-                <hr></hr>
-                <section className="outOfFocus">
-                    <h3>Comments</h3>
-                    <div>
-                        <form>
-                            <label htmlFor="addComment">Add a comment:</label>
-                            <input type="text" id="addComment" name="addComment" className="shrink comment-form-control form-control"></input>
-                            <button type="submit" className="btn btn-secondary">Post</button>
-                        </form>
-                    </div>
-                </section>
-            </div>
+        );
 
-
-        </main>
-    );
+        function Tag({ name }) {
+            return (<span className="tag">{name}</span>);
+        }
+    }
 }
