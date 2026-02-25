a26b28d80405e2b74a7e03701510443cd150056a
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-24 21:15:25 -0700

    actually idk if i want to go in this dir

--- src/database.js
+++ src/database.js
@@ -7,3 +7,26 @@ export function getTagsFromDatabase() {
 export function setTagsToDatabase(tags) {
     localStorage.setItem('tags', JSON.stringify(tags));
 }
+
+export function getStoriesFromDB() {
+    const j = lastStoryIndex();
+    const i = (Math.random() % j)++;
+    const
+
+    return JSON.parse(localStorage.getItem('tags')) || ['Grandma', 'Europe', 'Family-Friend Frank'];
+}
+
+export function addNewStoryToDB(storyObj) {
+    const name = 'Story' + lastStoryIndex()++;
+    localStorage.setItem(name, JSON.stringify(storyObj));
+}
+
+function lastStoryIndex() {
+    let keys = Object.keys(localStorage);
+    keys = keys.filter(key => key.includes("Story"));
+    if (keys.length == 0)
+        return 0;
+    console.log(keys);
+    const str = keys[keys.lastIndexOf()];
+    return Number(str[str.length - 1]);
+}
