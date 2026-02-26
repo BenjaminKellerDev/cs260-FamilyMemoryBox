29f03ba6b22c76200b50a2431d89f1da26f5adca
Benjamin Keller <Benjaminkeller@gmail.com>
2026-02-25 19:58:19 -0700

    object structure for comments

--- src/database.js
+++ src/database.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
-const defaultStory = "[{\"title\":\"That one time I went to that one place\",\"author\":\"Grandma\",\"storyTags\":[\"Grandma\",\"Europe\",\"Family-Friend Frank\"],\"story\":\"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.\"}]";
+const defaultStory = {
+    title: "That one time I went to that one place",
+    author: "Grandma",
+    postTime: 1772074405,
+    storyTags: ["Grandma", "Europe", "Family-Friend Frank"],
+    story: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.",
+    comments: [
+        {
+            "author": "Dad",
+            "text": "I remember that!!"
+        }, {
+            "author": "Grandson",
+            "text": "Wow! So cool! Is that where the turboencabulator you have in the garage came from?"
+        }
+    ]
+}
 export function getTagsFromDatabase() {
     return JSON.parse(localStorage.getItem('tags')) || ['Grandma', 'Europe', 'Family-Friend Frank'];
 }
@@ -10,10 +25,14 @@ export function setTagsToDatabase(tags) {
 }
 
 export function getStoriesFromDB() {
-    return JSON.parse(localStorage.getItem('Stories') || defaultStory);
+    return JSON.parse(localStorage.getItem('Stories')) || defaultStory;
 }
 
 export function addNewStoryToDB(storyObj) {
-    const storyList = JSON.parse(localStorage.getItem('Stories') || defaultStory)
+    const storyList = JSON.parse(localStorage.getItem('Stories')) || defaultStory;
     localStorage.setItem('Stories', JSON.stringify([...storyList, storyObj]));
+}
+
+export function addCommentToStory(commentObj) {
+
 }
