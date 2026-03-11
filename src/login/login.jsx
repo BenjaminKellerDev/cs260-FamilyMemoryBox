4c44a604ad4093d41f8c6a6f2a5313b19862bf9a
Benjamin Keller <Benjaminkeller@gmail.com>
2026-03-10 23:06:36 -0600

    work on new login for front end

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -19,24 +19,20 @@ export function Login({ setCurrentUser }) {
         }
     }
 
-    function createUser(e) {
+    async function createUser(e) {
         e.preventDefault();
-
-        //add to localStorage
-        var userArrayString = localStorage.getItem('userList') || '{"users":[]}';
-        var userArray = JSON.parse(userArrayString);
 
         if (passwordText.length === 0) {
             setErrorMSG('no password set');
         }
-        else if (!userArray.users.some((element) => element.nameText == nameText)) {
-            userArray.users.push({ nameText, passwordText });
-            localStorage.setItem('userList', JSON.stringify(userArray));
-
-            loginUser(e);
-        }
         else {
-            setErrorMSG('name already exists');
+            const response = await fetch('/api/auth/create', {
+                method: 'post',
+                body: JSON.stringify({ nameText: nameText, passwordText: passwordText }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                }
+            })
         }
     }
 
@@ -78,8 +74,8 @@ export function Login({ setCurrentUser }) {
                         <input type="password" id="passwordBox" name="loginPassword" className="shrink text-form-control form-control" onChange={passwordTextChange} />
                     </div>
                     <div>
-                        <button type="submit" className="margin10px btn btn-primary" onClick={loginUser}>Login</button>
-                        <button type="submit" className="margin10px btn btn-secondary" onClick={createUser}>Create</button>
+                        <button type="submit" className="margin10px btn btn-primary" onClick={() => loginUser()}>Login</button>
+                        <button type="submit" className="margin10px btn btn-secondary" onClick={() => createUser()}>Create</button>
                     </div>
                     <div className='errorMSG'> {errorMSG}</div>
                 </form>
