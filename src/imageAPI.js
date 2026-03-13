61ce90c042b4762af7a60d0fc49c894d17e8e969
Benjamin Keller <Benjaminkeller@gmail.com>
2026-03-12 22:41:04 -0600

    removed old api stuff

--- src/imageAPI.js
+++ src/imageAPI.js
@@ -5,31 +5,13 @@
 // Claude Monet = 35809
 
 export async function getImageFromAPI() {
-    const url = "https://api.artic.edu/api/v1/artworks/search?query[term][artist_ids]=35809&fields=id,title,image_id";
-    const params = {
-        method: 'get',
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8'
-        },
-        body: JSON.stringify({
+    const artistIDArray = [34767, 40642, 35809]
+    const selection = artistIDArray[Math.floor(Math.random() * artistIDArray.length)]
 
-        })
-    }
-    const response = await fetch(url, params);
-    console.log(response);
-    const array = [{
+    const array = {
         img: 'https://www.artic.edu/iiif/2/f8fd76e9-c396-5678-36ed-6a348c904d27/full/1686,/0/default.jpg',
         info: 'https://www.artic.edu/artworks/20684/paris-street-rainy-day',
         imageName: 'painting of paris street rainy day'
-    }, {
-        img: 'https://www.artic.edu/iiif/2/d1549113-7586-2193-de9f-80c1ce58ed64/full/1686,/0/default.jpg',
-        info: 'https://www.artic.edu/artworks/30361/entrance-to-the-port-of-honfleur',
-        imageName: 'painting of Entrance to the Port of Honfleur'
-    }, {
-        img: 'https://www.artic.edu/iiif/2/f38d16d0-7fa8-d6db-7922-3d0d057604c1/full/1686,/0/default.jpg',
-        info: 'https://www.artic.edu/artworks/33212/the-arcueil-aqueduct-at-sceaux-railroad-crossing',
-        imageName: 'painting of The Arcueil Aqueduct at Sceaux Railroad Crossing'
     }
-    ]
-    return array[Math.floor(Math.random() * array.length)];
+    return;
 }
