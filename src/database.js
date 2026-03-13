91c46a286550082c29de7a95aa31ddbe3e3157a7
Benjamin Keller <Benjaminkeller@gmail.com>
2026-03-12 18:09:23 -0600

    removed unused code

--- src/database.js
+++ src/database.js
@@ -1,24 +1,3 @@
-import React from 'react';
-
-const defaultStory = {
-    title: "That one time I went to that one place",
-    author: "Grandma",
-    postTime: 1772074405,
-    uuid: crypto.randomUUID(),
-    storyTags: ["Grandma", "Europe", "Family-Friend Frank"],
-    story: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis unde ipsa repellat iure vel ipsum nostrum molestiae distinctio facilis adipisci necessitatibus quasi tempora eveniet, quisquam saepe id? Natus, illum excepturi.",
-    comments: [
-        {
-            author: "Dad",
-            text: "I remember that!!",
-            uuid: crypto.randomUUID()
-        }, {
-            author: "Grandson",
-            text: "Wow! So cool! Is that where the turboencabulator you have in the garage came from?",
-            uuid: crypto.randomUUID()
-        }
-    ]
-}
 export async function getTagsFromDatabase() {
     return await endpointHandler('/api/tags', 'get');
 }
