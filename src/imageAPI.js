90df005b58045f0cc40d066d4949facb8da29dc0
Benjamin Keller <Benjaminkeller@gmail.com>
2026-03-12 22:54:07 -0600

    implemented 3rd party api

--- src/imageAPI.js
+++ src/imageAPI.js
@@ -1,5 +1,6 @@
 //https://api.artic.edu/api/v1/artworks?query[term][is_public_domain]=true&fields=id,title,image_id
 //https://api.artic.edu/api/v1/artworks/search?query[term][artist_ids]=35809&fields=id,title,image_id
+//selected impressionist artist with reasonable art
 // Jean Baptiste Armand Guillaumin = 34767
 // Johan Barthold Jongkind = 40642
 // Claude Monet = 35809
@@ -8,10 +9,20 @@ export async function getImageFromAPI() {
     const artistIDArray = [34767, 40642, 35809]
     const selection = artistIDArray[Math.floor(Math.random() * artistIDArray.length)]
 
-    const array = {
-        img: 'https://www.artic.edu/iiif/2/f8fd76e9-c396-5678-36ed-6a348c904d27/full/1686,/0/default.jpg',
-        info: 'https://www.artic.edu/artworks/20684/paris-street-rainy-day',
-        imageName: 'painting of paris street rainy day'
+    //call Art Institute of Chicago API for image metadata of all collected art from selected artist 
+    const url = 'https://api.artic.edu/api/v1/artworks/search?query[term][artist_ids]=' + selection + '&fields=id,title,image_id';
+    const response = await fetch(url, {
+        method: 'get'
+    })
+    const rJSON = await response.json();
+    const selectedPainting = rJSON.data[Math.floor(Math.random() * rJSON.data.length)];
+    console.log(selectedPainting);
+
+
+    const imgObj = {
+        img: 'https://www.artic.edu/iiif/2/' + selectedPainting.image_id + '/full/1686,/0/default.jpg',
+        info: 'https://www.artic.edu/artworks/' + selectedPainting.id,
+        imageName: selectedPainting.title
     }
-    return;
+    return imgObj;
 }
