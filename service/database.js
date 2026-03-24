496fb867c435e89bffb7775aee9860fc22c6cd2d
Benjamin Keller <Benjaminkeller@gmail.com>
2026-03-24 13:47:13 -0600

    connection test

--- service/database.js
+++ service/database.js
@@ -8,4 +8,13 @@ const dbUsers = db.collection('users');
 const dbTags = db.collection('tags');
 const dbStories = db.collection('stories');
 
-
+//connection test from simon
+(async function testConnection() {
+    try {
+        await db.command({ ping: 1 });
+        console.log("connected to database");
+    } catch (ex) {
+        console.log("connection to database is not working lol");
+        process.exit(1);
+    }
+})();
