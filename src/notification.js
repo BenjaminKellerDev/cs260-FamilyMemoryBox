ddf76a576912996924b33c4a855ed718e8936108
Benjamin Keller <Benjaminkeller@gmail.com>
2026-04-07 23:36:05 -0600

    fixed vite for dev environment

--- src/notification.js
+++ src/notification.js
@@ -1,7 +1,8 @@
 
 class NotificationSystem {
     constructor() {
-        let port = window.location.port;
+        //let port = window.location.port;
+        let port = 4000;
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
     }
