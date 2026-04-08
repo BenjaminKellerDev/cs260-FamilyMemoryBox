61eb09486ef4779f19565d1fe59a6bc00dd9a946
Benjamin Keller <Benjaminkeller@gmail.com>
2026-04-08 00:45:24 -0600

    fixed bug with deployment

--- src/notification.js
+++ src/notification.js
@@ -4,8 +4,8 @@ class NotificationSystem {
     handlers = [];
 
     constructor() {
-        //let port = window.location.port;
-        let port = 4000;
+        let port = window.location.port;
+        //let port = 4000;
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
         this.socket.onmessage = async (message) => {
