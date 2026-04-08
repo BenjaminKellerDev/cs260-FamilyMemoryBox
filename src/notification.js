9fb7ed49fbfae5358ec272a340dcd3cf972c8239
Benjamin Keller <Benjaminkeller@gmail.com>
2026-04-08 00:15:05 -0600

    moved object back to singleton like position

--- src/notification.js
+++ src/notification.js
@@ -1,18 +1,25 @@
 
 class NotificationSystem {
 
+    handlers = [];
 
-    constructor(onMessage) {
+    constructor() {
         //let port = window.location.port;
         let port = 4000;
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
         this.socket.onmessage = async (message) => {
-            onMessage();
+            this.handlers.forEach(element => {
+                element();
+            });
         }
+    }
+
+    addHandler(handel) {
+        this.handlers.push(handel);
     }
 
 
 }
-
-export { NotificationSystem }
+const notificationSystem = new NotificationSystem();
+export { notificationSystem }
--- src/stories/popup.jsx
+++ src/stories/popup.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import { NotificationSystem } from '../notification'
+import { notificationSystem } from '../notification'
 export function Popup({ refreshPosts }) {
     //websocket
 
     const [messageCount, updateCount] = React.useState(0);
-    const notificationSystem = new NotificationSystem(() => {
+    notificationSystem.addHandler(() => {
         updateCount(messageCount + 1);
     });
-
     const [newStoriesNotification, setNSN] = React.useState(false);
     React.useEffect(() => {
         if (messageCount > 0)
