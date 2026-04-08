dfbaca1f972bbbe111590bf6fd675089a6e11d14
Benjamin Keller <Benjaminkeller@gmail.com>
2026-04-07 20:34:57 -0600

    added WebSocket auto ping based on simon

--- service/newPostBroadcaster.js
+++ service/newPostBroadcaster.js
@@ -1,9 +1,22 @@
-const { WebSocketServer } = require('ws');
+const { WebSocketServer, WebSocket } = require('ws');
 
 
 function newPostBroadcaster(app) {
 
     const socketServer = new WebSocketServer({ server: app });
+
+    socketServer.on('connection', (socket) => {
+        socket.isAlive = true;
+
+        socket.on('message', function message() {
+            socketServer.clients.forEach((client) => {
+                if (client !== socket && client.readyState == WebSocket.OPEN) {
+                    client.send("update pls")// replace with story data??
+                }
+
+            });
+        });
+    })
 }
 
 module.exports = {
